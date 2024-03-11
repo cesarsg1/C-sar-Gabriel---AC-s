d6b9cf424097e4ede323dc1935c538f2d1d3068d
cesarsg1 <161887405+cesarsg1@users.noreply.github.com>
2024-03-10 18:25:57 -0700

    Add files via upload

---
+++ AC1/César Gabriel - AC1.py
@@ -0,0 +1,15 @@
+"""
+print("Para calcular as raízes da equação de segundo grau ax^2 + bx + c: \n")
+a = int(input("insira o parâmetro a da equação: "))
+b = int(input("insira o parâmetro b da equação: "))
+c = int(input("insira o parâmetro c da equação: "))
+
+x1 = (-b + (((b ** 2) - 4 * a * c) ** 0.5)) / 2 * a
+x2 = (-b - (((b ** 2) - 4 * a * c) ** 0.5)) / 2 * a
+
+print(4**0.5)
+
+#print("A primeira raíz da equação é ", x1)
+#print("A segunda raíz da equação é ", x2)
+
+"""
